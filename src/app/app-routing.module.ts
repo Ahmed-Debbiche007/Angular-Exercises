8f304f8fb27d95aad5dc67e085dfecfbf86c3f45
Ahmed-Debbiche007 <ahmed.debbiche@esprit.tn>
2023-02-08 10:49:45 +0100

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductsComponentComponent } from './products-component/products-component.component';
+import { OffresEmploiComponentComponent } from './offres-emploi-component/offres-emploi-component.component';
+import { ArticlesComponentComponent } from './articles-component/articles-component.component';
+import { PageNotFoundComponentComponent } from './page-not-found-component/page-not-found-component.component';
+import { AppComponent } from './app.component';
+import { HomeComponentComponent } from './home-component/home-component.component';
+import { ProductDetailComponentComponent } from './product-detail-component/product-detail-component.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home', component: HomeComponentComponent, children: [
+      { path: 'products', component: ProductsComponentComponent },
+      { path: 'products/:id', component: ProductDetailComponentComponent },
+      { path: 'offers', component: OffresEmploiComponentComponent },
+      { path: 'articles', component: ArticlesComponentComponent },
+    ]
+  },
+  {path:'', redirectTo:'home', pathMatch:'full'},
+  { path: '**', component: PageNotFoundComponentComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,9 @@ import { OffresEmploiComponentComponent } from './offres-emploi-component/offres
 import { ArticlesComponentComponent } from './articles-component/articles-component.component';
 import { NavbarComponentComponent } from './navbar-component/navbar-component.component';
 import { ProductsComponentComponent } from './products-component/products-component.component';
+import { PageNotFoundComponentComponent } from './page-not-found-component/page-not-found-component.component';
+import { HomeComponentComponent } from './home-component/home-component.component';
+import { ProductDetailComponentComponent } from './product-detail-component/product-detail-component.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +17,10 @@ import { ProductsComponentComponent } from './products-component/products-compon
     OffresEmploiComponentComponent,
     ArticlesComponentComponent,
     NavbarComponentComponent,
-    ProductsComponentComponent
+    ProductsComponentComponent,
+    PageNotFoundComponentComponent,
+    HomeComponentComponent,
+    ProductDetailComponentComponent
   ],
   imports: [
     BrowserModule,
